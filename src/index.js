6306b20f7d82b66b979559d2316ca749bf25b78c
Yosef da Iry <yosefda@gmail.com>
2019-07-13 09:29:48 +1000

    Create API structure.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('test');
+const express = require('express');
+const api = express();
+const bodyParser = require("body-parser");
+const logger = require("./logger");
+
+const listenPort = parseInt(process.env.LISTEN_PORT) || 9999;
+
+// To parse POST payload.
+api.use(bodyParser.json());
+
+
+// This API only has 1 endpoint to send email.
+api.post('/v1/send', (req, res) => {
+    console.log('/v1/send');
+    res.send();
+});
+
+
+// Starts API.
+api.listen(listenPort, () => {
+    logger.info('API running on port: ' + String(listenPort));
+});
