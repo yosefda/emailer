7a796bcf475f2645b4a39689562a2817ff8024d2
Yosef da Iry <yosefda@gmail.com>
2019-07-14 10:44:37 +1000

    Implement email module and add tests.

--- tests/email.js
+++ tests/email.js
@@ -3,8 +3,109 @@
 const { expect } = require('chai');
 const email = require('../src/email');
 
-describe('Test email', () => {
-    it('creates email', () => {
-        const emailObj = email.createEmail({ to: 'u@u.com' });
+describe('Test create email object', () => {
+    it('throws error when missing From', () => {
+        expect(() => email.createEmail({})).to.throw('Missing From');
+        expect(() => email.createEmail({ from: '' })).to.throw('Missing From');
+    });
+
+    it('throws error when missing To', () => {
+        expect(() => email.createEmail({ from: 'bob@example.com' })).to.throw('Missing To');
+        expect(() => email.createEmail({ from: 'bob@example.com', to: '' })).to.throw('Missing To');
+    });
+
+    it('throws error when From contains invalid email', () => {
+        expect(() => email.createEmail({ from: 'bob@example', to: 'sam@example.com' })).to.throw(
+            'Invalid email bob@example'
+        );
+    });
+
+    it('throws error when To contains invalid email', () => {
+        expect(() => email.createEmail({ from: 'bob@example.com', to: 'sam@example.com, jane@example' })).to.throw(
+            'Invalid email jane@example'
+        );
+        expect(() => email.createEmail({ from: 'bob@example.com', to: 'sam@example.com, jane@example, ' })).to.throw(
+            'Invalid email '
+        );
+    });
+
+    it('throws error when CC contains invalid email', () => {
+        expect(() =>
+            email.createEmail({
+                from: 'bob@example.com',
+                to: 'sam@example.com, jane@example.com',
+                cc: 'joe@example',
+            })
+        ).to.throw('Invalid email joe@example');
+    });
+
+    it('throws error when BCC contains invalid email', () => {
+        expect(() =>
+            email.createEmail({
+                from: 'bob@example.com',
+                to: 'sam@example.com, jane@example.com',
+                cc: 'joe@example.com',
+                bcc: 'smith@example',
+            })
+        ).to.throw('Invalid email smith@example');
+    });
+
+    it('throws error when Subject is not string type', () => {
+        expect(() =>
+            email.createEmail({
+                from: 'bob@example.com',
+                to: 'sam@example.com, jane@example.com',
+                cc: 'joe@example.com',
+                bcc: 'smith@example.com',
+                subject: {},
+            })
+        ).to.throw('Email Subject must be a string');
+    });
+
+    it('throws error when Body is not string type', () => {
+        expect(() =>
+            email.createEmail({
+                from: 'bob@example.com',
+                to: 'sam@example.com, jane@example.com',
+                cc: 'joe@example.com',
+                bcc: 'smith@example.com',
+                subject: 'Test email',
+                body: [],
+            })
+        ).to.throw('Email Body must be a string');
+    });
+
+    it('returns email', () => {
+        const emailInfo = email.createEmail({
+            from: 'bob@example.com',
+            to: 'sam@example.com, jane@example.com',
+            cc: 'joe@example.com',
+            bcc: 'smith@example.com',
+            subject: 'Test email',
+            body: 'Hi there guys!',
+        });
+
+        expect(emailInfo.getFrom()).to.equal('bob@example.com');
+        expect(emailInfo.getTo()).to.deep.equal(['sam@example.com', 'jane@example.com']);
+        expect(emailInfo.getCc()).to.deep.equal(['joe@example.com']);
+        expect(emailInfo.getBcc()).to.deep.equal(['smith@example.com']);
+        expect(emailInfo.getSubject()).to.equal('Test email');
+        expect(emailInfo.getBody()).to.equal('Hi there guys!');
+    });
+
+    it('returns email', () => {
+        const emailInfo = email.createEmail({
+            from: 'bob@example.com',
+            to: 'sam@example.com, jane@example.com',
+            cc: 'joe@example.com',
+            bcc: 'smith@example.com',
+        });
+
+        expect(emailInfo.getFrom()).to.equal('bob@example.com');
+        expect(emailInfo.getTo()).to.deep.equal(['sam@example.com', 'jane@example.com']);
+        expect(emailInfo.getCc()).to.deep.equal(['joe@example.com']);
+        expect(emailInfo.getBcc()).to.deep.equal(['smith@example.com']);
+        expect(emailInfo.getSubject()).to.equal('');
+        expect(emailInfo.getBody()).to.equal('');
     });
 });
